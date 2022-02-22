ac962418bcffbb0984d1d5f0d63d5e0dec952d52
matt <mat@unc.edu>
2022-02-22 17:10:31 -0500

    fixed a lot of tests

--- flips.js
+++ flips.js
@@ -4,8 +4,14 @@ import {coinFlips} from "./modules/coin.mjs";
 
 // const args = require('minimist')(process.argv.slice(2));
 
-import * as minimist from './node_modules/minimist/index.js';
+// import * as minimist from './node_modules/minimist/index.js';
 
-const callString = process.argv.slice(2);
+let callString = process.argv.slice(2);
 // // Call the coinFlip function and put the return into STDOUT
+if(callString.length == 0) {
+    callString = '1';
+} else if (callString.toString().includes('=')) {
+    const index = callString.toString().indexOf('=');
+    callString = callString.toString().substring(index+1);
+}
 console.log(coinFlips(callString));
--- guess-flip.js
+++ guess-flip.js
@@ -1,8 +1,22 @@
 // Import the coinFlip function from your coin.mjs file
 import {flipACoin} from "./modules/coin.mjs";
-import ('minimist')(process.argv.slice(2));
+// import ('minimist')(process.argv.slice(2));
 
-args["call"];
+let callString = process.argv.slice(2);
+
+// args["call"];
+
+if(callString.length == 0) {
+    console.log("Error: no input.")
+} else if (callString.toString().includes('=')) {
+    const index = callString.toString().indexOf('=');
+    let callSubString = callString.toString().substring(index+1);
+    if(!(callSubString == 'heads' || callSubString == 'tails')) {
+        console.log("Usage: node guess-flip.js --call='heads'");
+    } else {
+        console.log(flipACoin(callSubString));
+    }
+}
 
 // Call the coinFlip function and put the return into STDOUT
-console.log(flipACoin(args.call));
+// console.log(flipACoin(args.call));
